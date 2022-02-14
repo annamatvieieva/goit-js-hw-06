25724f66d25f6eb086432dccb26c293746db1e1f
Anna Matvieieva <90278772+annamatvieieva@users.noreply.github.com>
2022-02-14 21:21:53 +0200

    add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonChange = document.querySelector('.change-color');
+const bodyOfDocument = document.querySelector('body');
+const color = document.querySelector('.color');
+
+
+buttonChange.addEventListener('click', (event) => {
+  const colorRandom = getRandomHexColor();
+  bodyOfDocument.style.backgroundColor = colorRandom;
+  color.textContent = colorRandom;
+})
+
